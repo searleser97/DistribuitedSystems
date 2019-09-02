9b671bf74a872a99f7b695e8ee76b47d4d32a1eb
searleser97 <gabrielsanv97@gmail.com>
2019-09-01 22:01:11 -0500

    updated on 09/01/2019 at 10:01:11 PM

---
+++ Proyectos/proj1.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double epsilon = 1e-4;
+double Sqrt(double n) {
+  int digitCount = log10(n) + 1;
+  double b = 2 * pow(10, ((int)n & 1) ? ceil((digitCount - 1) / 2)
+                                      : floor((digitCount - 1) / 2));
+  double h = n / b;
+  while (abs(n - b * b) > epsilon) {
+    cout << b << " ";
+    b = (h + b) / 2;
+    h = n / b;
+  }
+  return b;
+}
+
+int main() {
+  double n;
+  cin >> n;
+  cout << Sqrt(n) << endl;
+  return 0;
+}
