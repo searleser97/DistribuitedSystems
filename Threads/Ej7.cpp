bd4be43169c1cd96c741c696013d934502fffe58
alaneos777 <alan-enrique@live.com.mx>
2019-09-03 09:54:49 -0500

    Update

--- Threads/Ej7.cpp
+++ Threads/Ej7.cpp
@@ -8,28 +8,26 @@ using namespace std;
 
 int global = 10;
 int n_hilos = 3;
-vector<Semaforo> sems;
+vector<Semaforo> sems(n_hilos);
 vector<thread> hilos;
 
 void funcion (int hilo) {
   while (global > 0) {
     sems[hilo].wait();
-    printf("Soy el hilo %d, y esta es la impresion %d\n", hilo, global--);
-    for (int i = 0; i < n_hilos; ++i) {
-      if (i != hilo) {
-        sems[i].post();
-      }
-    }
+    if (global >= 0)
+      printf("Soy el hilo %d, y esta es la impresion %d\n", hilo, global--);
+    sems[(hilo + 1) % n_hilos].post();
   }
 }
 
 int main () {
   //Inicializa los semaforos
   for (int i = 0; i < n_hilos; ++i) {
-    hilos.emplace_back(funcion, i);
-    sems.emplace_back(0);
     if (i == 0) sems[i].init(1);
     else sems[i].init(0);
+  }
+  for (int i = 0; i < n_hilos; ++i) {
+    hilos.emplace_back(funcion, i);
   }
   for (auto & hilo : hilos) {
     hilo.join();
