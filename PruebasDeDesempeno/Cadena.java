63081b46cb9e9e2450c68eb8168b4da52380e9f8
Juan Carlos García Medina <Carlos_isc7@outlook.com>
2019-09-03 09:56:35 -0500

    Fix identation

--- PruebasDeDesempeno/Cadena.java
+++ PruebasDeDesempeno/Cadena.java
@@ -3,14 +3,13 @@ import java.util.Scanner;
 
 public class Cadena {
 
-	public static void main(String []args)
-	{
+	public static void main(String []args) {
 		String cadenota = "";
 		int n = 17576 * 10;
 		int k = 0, m = 3;
+		
 		Random rnd = new Random();
-		while(k < n)
-		{
+		while(k < n) {
 			String cadena = "";
 			for(int i = k; i < k + m; i++) {
 				cadena += ( (char) (rnd.nextDouble() * 26 + 65)) + "";
@@ -18,18 +17,16 @@ public class Cadena {
 			if(k != 0)
 				cadenota += " ";
 			k += m;
-
 			cadenota += cadena;
 		}
-		//System.out.println(cadenota);
 
 		String strFind = "IPN";
-        int count = 0, fromIndex = 0;
-        
-        while ((fromIndex = cadenota.indexOf(strFind, fromIndex)) != -1 ) {
-            count++;
-            fromIndex++;           
-        }
-        System.out.println("cadena IPN encontrada " + count + " veces.");
+		int count = 0, fromIndex = 0;
+
+		while ((fromIndex = cadenota.indexOf(strFind, fromIndex)) != -1 ) {
+		    count++;
+		    fromIndex++;           
+		}
+		System.out.println("cadena IPN encontrada " + count + " veces.");
 	}
 }
