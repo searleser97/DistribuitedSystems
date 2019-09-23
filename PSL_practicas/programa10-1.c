8f6d1bd57a8eb7619e91b18b661b110fbacd33de
searleser97 <gabrielsanv97@gmail.com>
2019-09-23 11:25:55 -0500

    updated on 09/23/2019 at 11:25:55 AM

--- PSL_practicas/programa10-1.c
+++ PSL_practicas/programa10-1.c
@@ -7,6 +7,15 @@
 
 int puerto = 7200;
 
+uint32_t ipBlock(uint32_t x, int pos)
+{
+   return (x >> (pos * 8)) & 0xFF;
+}
+
+void printIp(uint32_t ip) {
+   printf("%u.%u.%u.%u", ipBlock(ip, 0), ipBlock(ip, 1), ipBlock(ip, 2), ipBlock(ip, 3));
+}
+
 int main(void)
 {
    struct sockaddr_in msg_to_server_addr, client_addr;
@@ -16,14 +25,15 @@ int main(void)
    /* rellena la dirección del servidor */
    bzero((char *)&msg_to_server_addr, sizeof(msg_to_server_addr));
    msg_to_server_addr.sin_family = AF_INET;
-   msg_to_server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+   uint32_t ip = msg_to_server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
    msg_to_server_addr.sin_port = htons(puerto);
-   
    /* rellena la direcciòn del cliente*/
    bzero((char *)&client_addr, sizeof(client_addr));
    client_addr.sin_family = AF_INET;
    client_addr.sin_addr.s_addr = INADDR_ANY;
-   
+   printf("ip del servidor a la que nos conectaremos: ");
+   printIp(ip);
+   printf("\n");
    /*cuando se utiliza por numero de puerto el 0, el sistema se encarga de asignarle uno */
    client_addr.sin_port = htons(0);
    bind(s, (struct sockaddr *)&client_addr,sizeof(client_addr));
@@ -32,7 +42,13 @@ int main(void)
    sendto(s, (char *)num, 2 * sizeof(int), 0, (struct sockaddr *) &msg_to_server_addr, sizeof(msg_to_server_addr));
    
    /* se bloquea esperando respuesta */
-   recvfrom(s, (char *)&res, sizeof(int), 0, NULL, NULL);
+   struct sockaddr_in sender;
+   int senderSize = sizeof(sender);
+   recvfrom(s, (char *)&res, sizeof(int), 0, (struct sockaddr*) &sender, &senderSize);
+   printf("puerto de servidor: %u\n", ntohs(sender.sin_port));
+   printf("ip del servidor que nos manda la suma: ");
+   printIp(sender.sin_addr.s_addr);
+   printf("\n");
    printf("2 + 5 = %d\n", res);
    close(s);
 }
--- PSL_practicas/programa9-1.c
+++ PSL_practicas/programa9-1.c
@@ -1,33 +1,48 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <stdio.h>
-#include <netinet/in.h>
 #include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
 #include <strings.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+uint32_t ipBlock(uint32_t x, int pos)
+{
+   return (x >> (pos * 8)) & 0xFF;
+}
+
+void printIp(uint32_t ip) {
+   printf("%u.%u.%u.%u", ipBlock(ip, 0), ipBlock(ip, 1), ipBlock(ip, 2), ipBlock(ip, 3));
+}
 
 int puerto = 7200;
 
-int main(void)
-{
-   int num[2];
-   int s, res, clilen;
-   struct sockaddr_in server_addr, msg_to_client_addr;
-   
-   s = socket(AF_INET, SOCK_DGRAM, 0);
-   
-   /* se asigna una direccion al socket del servidor*/
-   bzero((char *)&server_addr, sizeof(server_addr));
-   server_addr.sin_family = AF_INET;
-   server_addr.sin_addr.s_addr = INADDR_ANY;
-   server_addr.sin_port = htons(puerto);
-   bind(s, (struct sockaddr *)&server_addr, sizeof(server_addr));
-   clilen = sizeof(msg_to_client_addr);
-   while(1) {
-      recvfrom(s, (char *) num, 2*sizeof(int), 0, (struct sockaddr *)&msg_to_client_addr, &clilen);
-      res = num[0] + num[1];
-      
-      /* envía la petición al cliente. La estructura msg_to_client_addr contiene la dirección socket del cliente */
-      sendto(s, (char *)&res, sizeof(int), 0, (struct sockaddr *)&msg_to_client_addr, clilen);
-   }
-}
+int main(void) {
+  int num[2];
+  int s, res, clilen;
+  struct sockaddr_in server_addr, msg_to_client_addr;
 
+  s = socket(AF_INET, SOCK_DGRAM, 0);
+
+  /* se asigna una direccion al socket del servidor*/
+  bzero((char *)&server_addr, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(puerto);
+  bind(s, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  clilen = sizeof(msg_to_client_addr);
+  while (1) {
+    recvfrom(s, (char *)num, 2 * sizeof(int), 0,
+             (struct sockaddr *)&msg_to_client_addr, &clilen);
+    
+    printf("puerto de cliente: %u\n", ntohs(msg_to_client_addr.sin_port));
+    printf("ip del cliente que nos solicita la suma: ");
+    printIp(msg_to_client_addr.sin_addr.s_addr);
+    printf("\n");
+    res = num[0] + num[1];
+
+    /* envía la petición al cliente. La estructura msg_to_client_addr contiene
+     * la dirección socket del cliente */
+    sendto(s, (char *)&res, sizeof(int), 0,
+           (struct sockaddr *)&msg_to_client_addr, clilen);
+  }
+}
