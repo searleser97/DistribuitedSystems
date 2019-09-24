604974839133b3c3941c8b88453e5b03c86d58c3
alaneos777 <alan-enrique@live.com.mx>
2019-09-24 09:10:17 -0500

    Update

--- UDP_C++/PaqueteDatagrama.h
+++ UDP_C++/PaqueteDatagrama.h
@@ -1,5 +1,24 @@
-class PaqueteDatagrama{
+#ifndef PaqueteDatagrama_H_
+#define PaqueteDatagrama_H_
+
+class PaqueteDatagrama {
 	public:
-		PaqueteDatagrama(char * datos, unsigned int len, char * ip, int puerto);
-		PaqueteDatagrama(unsigned int len);
-}
+		PaqueteDatagrama(const char * datos, unsigned int longitud, const char * ip, int puerto);
+		PaqueteDatagrama(unsigned int longitud);
+		~PaqueteDatagrama();
+		char *obtieneDireccion();
+		unsigned int obtieneLongitud();
+		int obtienePuerto();
+		char *obtieneDatos();
+		void inicializaPuerto(int puerto);
+		void inicializaIp(const char * ip);
+		void inicializaDatos();
+		void inicializaDatos(const char * datos);
+	private:
+		char *datos;
+		char ip[16];
+		unsigned int longitud;
+		int puerto;
+};
+
+#endif
