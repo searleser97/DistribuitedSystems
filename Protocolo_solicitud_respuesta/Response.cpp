8ef4293ef22942f72326551e5f94560c2b2d3bc9
Juan Carlos Garcia Medina <carlos_isc7@outlook.com>
2019-10-01 09:55:58 -0500

    server side

--- Protocolo_solicitud_respuesta/Response.cpp
+++ Protocolo_solicitud_respuesta/Response.cpp
@@ -1,3 +1,36 @@
-Respuesta(int pl);
-        struct mensaje *getRequest(void);
-        void sendReply(char *respuesta);
+#include "Response.h"
+#include <string.h>
+
+Respuesta::Respuesta(int port)
+{
+   socketlocal = new SocketDatagrama(port);
+
+}
+Message *Respuesta::getRequest()
+{
+    PaqueteDatagrama pq(sizeof(Message));
+    Message *response;
+
+    socketlocal->recibe(pq);
+    strcpy(IP, pq.obtieneDireccion());
+    port = pq.obtienePuerto();
+
+    Message* msg = (Message*) pq.obtieneDatos();
+    operation = msg->operationId;
+    requestId = msg->requestId;
+
+    return msg;
+}
+
+void Respuesta::sendReply(const char *respuesta)
+{
+    Message *msg = new Message();
+    memcpy(msg->arguments, respuesta, sizeof(msg->arguments));
+    msg->messageType = Code::respuesta;
+    msg->operationId = operation;
+    msg->requestId = requestId;
+    PaqueteDatagrama paquete((char*) msg, sizeof(msg), IP, port );
+    
+    socketlocal->envia(paquete);
+
+}
--- Protocolo_solicitud_respuesta/Response.h
+++ Protocolo_solicitud_respuesta/Response.h
@@ -1,10 +1,19 @@
+#ifndef RESPONSE_H
+#define RESPONSE_H
 #include "SocketDatagrama.h"
+#include "Message.h"
 
 class Respuesta {
     public:
         Respuesta(int pl);
-        struct mensaje *getRequest(void);
-        void sendReply(char *respuesta);
+        Message *getRequest(void);
+        void sendReply(const char *respuesta);
+        char *IP;
+        int port;
     private:
         SocketDatagrama *socketlocal;
+        Code::allowedOperations operation;
+        unsigned int requestId;
 };
+
+#endif
