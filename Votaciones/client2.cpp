f27ce6ee76c5bcd932e9366fbf9c77e6c19882af
Juan Carlos Garcia Medina <carlos_isc7@outlook.com>
2019-11-12 08:59:57 -0600

    votaciones with timestamp

--- Votaciones/client2.cpp
+++ Votaciones/client2.cpp
@@ -4,6 +4,14 @@
 #include <iostream>
 #include <random>
 #include <string>
+#include <sys/time.h>
+
+// struct timeval {
+// 	time_t tv_sec; /* segundos */
+// 	suseconds_t tv_usec; /* microsegundos */
+// };
+
+typedef struct timeval TimeVal;
 
 using namespace std;
 
@@ -25,9 +33,11 @@ int main(int argc, char *argv[]) {
 	int n = atoi(argv[1]);
 	FILE *f = fopen(argv[2], "rb");
 	registro reg;
+	TimeVal tv;
 	while (n-- && fscanf(f, "%10s%18s%3s", reg.celular, reg.CURP, reg.partido) != EOF) {
 		try {
-			char res = *r.doOperation(ip, puerto, Message::allowedOperations::registerVote, (char *)&reg, sizeof(reg), len_reply);
+			tv = *(TimeVal*)r.doOperation(ip, puerto, Message::allowedOperations::registerVote, (char *)&reg, sizeof(reg), len_reply);
+			cout <<"ss:ms "<< tv.tv_sec << ":" << tv.tv_usec << '\n';
 			/*if (res) {
 				cout << "Voto registrado correctamente\n";
 			} else {
--- Votaciones/server.cpp
+++ Votaciones/server.cpp
@@ -3,6 +3,14 @@
 #include <string>
 #include <unordered_map>
 #include <bits/stdc++.h>
+#include <sys/time.h>
+
+// struct timeval {
+// 	time_t tv_sec; /* segundos */
+// 	suseconds_t tv_usec; /* microsegundos */
+// };
+
+typedef struct timeval TimeVal;
 
 using namespace std;
 
@@ -31,7 +39,7 @@ int main(int argc, char *argv[]) {
 	
 	f = fopen(argv[1], "rb+");
 	if (f) {
-		while(fscanf(f, "%10s%18s%3s", reg.celular, reg.CURP, reg.partido) != EOF){
+		while(fscanf(f, "%10s%18s%3s", reg.celular, reg.CURP, reg.partido) != EOF) {
 			nbd.insert({reg.celular, reg.CURP, reg.partido});
 		}
 		fclose(f);
@@ -43,20 +51,24 @@ int main(int argc, char *argv[]) {
 	cin >> puerto;
 	Reply reply(puerto);
 	cout << "Servidor iniciado...\n";
-
+	TimeVal tv;
 	while (1) {
 		char res = 0;
 		Message *msg = reply.getRequest();
 		reg = *(registro*)msg->arguments;
+		tv.tv_sec = 0;
+		tv.tv_usec = 0;
 		if (msg->operationId == Message::allowedOperations::registerVote) {
+			
 			tuple<string, string, string> t(reg.celular, reg.CURP, reg.partido);
 			if(nbd.find(t) == nbd.end()){
+				gettimeofday(&tv, NULL);//get time
 				nbd.insert(t);
 				fprintf(f, "%s%s%s\n", reg.celular, reg.CURP, reg.partido);
 				fflush(f);
 				res = 1;
 			}
-			reply.sendReply(&res, sizeof(res));
+			reply.sendReply((char*)&tv, sizeof(tv));
 		}
 	}
 	return 0;
